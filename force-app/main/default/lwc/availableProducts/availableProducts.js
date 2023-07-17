fd8fa29fbda483dad7491d934dc613eaa1f9166f
Alfred Chavez <achavezurday@gmail.com>
2023-07-17 11:54:33 -0500

    Implemented available products, get and display on lwc

--- force-app/main/default/lwc/availableProducts/availableProducts.js
+++ force-app/main/default/lwc/availableProducts/availableProducts.js
@@ -1,3 +1,53 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from "lwc";
+import getAvailableProducts from "@salesforce/apex/AvailableProductsController.getAvailableProducts";
+import addProductToOrder from "@salesforce/apex/AvailableProductsController.addProductToOrder"
 
-export default class AvailableProducts extends LightningElement {}
+const ROW_ACTIONS = [
+  {label: "Add to Order", name: "add_to_order"}
+];
+
+const DATA_COLUMNS = [
+  { label: "Name", fieldName: "name", type: "text" },
+  { label: "List Price", fieldName: "list_price", type: "currency" },
+  {type: "action", typeAttributes: {rowActions: ROW_ACTIONS}}
+];
+
+export default class AvailableProducts extends LightningElement {
+  @track data = [];
+  columns = DATA_COLUMNS;
+  @api recordId;
+
+  connectedCallback() {
+    getAvailableProducts({ orderId: this.recordId })
+      .then((result) => {
+        this.data = result.map((pbe) => ({
+          name: pbe.name,
+          list_price: pbe.price,
+          id: pbe.id
+        }));
+      })
+      .catch((err) => console.error("Some err", err));
+  }
+
+  handleAddToOrder(row) {
+    const productId = row.id;
+    const orderId = this.recordId;
+    console.log("Add", productId, "TO", orderId)
+    addProductToOrder({productId, orderId})
+      .then(() => {
+        console.log("finished to add :)")
+      })
+      .catch(err => console.error(err))
+  }
+  handleRowAction(event) {
+    const actionName = event.detail.action.name;
+    const row = event.detail.row;
+    switch (actionName) {
+      case "add_to_order":
+        this.handleAddToOrder(row);
+        break;
+      default:
+        throw Error("Action not implemented yet");
+    }
+  }
+}
